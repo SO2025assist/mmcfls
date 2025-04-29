2122448329840c4bcd37d07b68d9ae3342c0483a
SO2025assist <loboscoemil@hotmail.com>
2025-04-29 08:16:23 -0700

    Update upper_plate_fall.js

--- deveopment_behavior_packs/engineer building models/scripts/upper_plate_fall.js
+++ deveopment_behavior_packs/engineer building models/scripts/upper_plate_fall.js
@@ -1,17 +1,17 @@
-import { world, Vector3, BlockPermutation, system } from '@minecraft/server';
+import { world, BlockPermutation, system } from '@minecraft/server';
 
 // Check for unsupported upper_plate blocks on block updates
-world.afterEvents.blockUpdate.subscribe((event) => {
-  const block = event.block;
+world.afterEvents.blockUpdate.subscribe(function (event) {
+  var block = event.block;
   if (block.typeId === 'engineer:upper_plate') {
-    const belowPos: Vector3 = { x: block.x, y: block.y - 1, z: block.z };
-    const blockBelow = block.dimension.getBlock(belowPos);
+    var belowPos = { x: block.x, y: block.y - 1, z: block.z };
+    var blockBelow = block.dimension.getBlock(belowPos);
 
     // If no block below or it's air, make the block fall
     if (!blockBelow || blockBelow.isAir) {
-      system.run(() => {
+      system.run(function () {
         // Spawn a falling block entity
-        const fallingBlock = block.dimension.spawnEntity('minecraft:falling_block', {
+        var fallingBlock = block.dimension.spawnEntity('minecraft:falling_block', {
           x: block.x + 0.5,
           y: block.y,
           z: block.z + 0.5
@@ -28,16 +28,16 @@ world.afterEvents.blockUpdate.subscribe((event) => {
 });
 
 // Handle block placement to check for immediate falling
-world.afterEvents.blockPlace.subscribe((event) => {
-  const block = event.block;
+world.afterEvents.blockPlace.subscribe(function (event) {
+  var block = event.block;
   if (block.typeId === 'engineer:upper_plate') {
-    const belowPos: Vector3 = { x: block.x, y: block.y - 1, z: block.z };
-    const blockBelow = block.dimension.getBlock(belowPos);
+    var belowPos = { x: block.x, y: block.y - 1, z: block.z };
+    var blockBelow = block.dimension.getBlock(belowPos);
 
     // If no block below or it's air, make the block fall
     if (!blockBelow || blockBelow.isAir) {
-      system.run(() => {
-        const fallingBlock = block.dimension.spawnEntity('minecraft:falling_block', {
+      system.run(function () {
+        var fallingBlock = block.dimension.spawnEntity('minecraft:falling_block', {
           x: block.x + 0.5,
           y: block.y,
           z: block.z + 0.5
@@ -50,14 +50,14 @@ world.afterEvents.blockPlace.subscribe((event) => {
 });
 
 // Handle falling block landing
-world.afterEvents.entitySpawn.subscribe((event) => {
-  const entity = event.entity;
+world.afterEvents.entitySpawn.subscribe(function (event) {
+  var entity = event.entity;
   if (entity.typeId === 'minecraft:falling_block') {
-    const blockType = entity.getDynamicProperty('block_type');
+    var blockType = entity.getDynamicProperty('block_type');
     if (blockType === 'engineer:upper_plate') {
-      system.run(() => {
-        const pos: Vector3 = { x: Math.floor(entity.x), y: Math.floor(entity.y), z: Math.floor(entity.z) };
-        const dimension = entity.dimension;
+      system.run(function () {
+        var pos = { x: Math.floor(entity.x), y: Math.floor(entity.y), z: Math.floor(entity.z) };
+        var dimension = entity.dimension;
         
         // Place the upper_plate block
         dimension.setBlockType(pos, 'engineer:upper_plate');
@@ -70,19 +70,19 @@ world.afterEvents.entitySpawn.subscribe((event) => {
 });
 
 // Handle block breaking to collapse unsupported upper_plate blocks above
-world.afterEvents.blockBreak.subscribe((event) => {
-  const block = event.block;
+world.afterEvents.blockBreak.subscribe(function (event) {
+  var block = event.block;
   if (block.typeId === 'engineer:upper_plate') {
-    let abovePos: Vector3 = { x: block.x, y: block.y + 1, z: block.z };
-    let maxHeight = 100; // Prevent infinite loops
+    var abovePos = { x: block.x, y: block.y + 1, z: block.z };
+    var maxHeight = 100; // Prevent infinite loops
     while (maxHeight > 0) {
-      const aboveBlock = block.dimension.getBlock(abovePos);
+      var aboveBlock = block.dimension.getBlock(abovePos);
       if (!aboveBlock || aboveBlock.typeId !== 'engineer:upper_plate') {
         break;
       }
-      system.run(() => {
+      system.run(function () {
         // Spawn falling block for the unsupported block
-        const fallingBlock = block.dimension.spawnEntity('minecraft:falling_block', {
+        var fallingBlock = block.dimension.spawnEntity('minecraft:falling_block', {
           x: aboveBlock.x + 0.5,
           y: aboveBlock.y,
           z: aboveBlock.z + 0.5
